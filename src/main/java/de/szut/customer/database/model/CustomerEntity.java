ed0a1ce7a6a7a6b48bd93f3db8069aa4748a2def
Lars Michaelis <mail@larmic.de>
2021-06-18 07:41:11 +0200

    feat: add jpa annotations to CustomerEntity.java

--- src/main/java/de/szut/customer/database/model/CustomerEntity.java
+++ src/main/java/de/szut/customer/database/model/CustomerEntity.java
@@ -1,23 +1,37 @@
 package de.szut.customer.database.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "customer")
 public class CustomerEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_generator")
+    @SequenceGenerator(name = "customer_generator", sequenceName = "customer_id_seq", allocationSize = 1)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "company", nullable = false)
     private String company;
+
+    @Column(name = "create_date", nullable = false)
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @Column(name = "last_update_date", nullable = false)
+    private LocalDateTime lastUpdateDate = LocalDateTime.now();
+
+    public CustomerEntity() {
+        // required by JPA
+    }
 
     public CustomerEntity(String name, String company) {
         this.name = name;
         this.company = company;
     }
-
-    private LocalDateTime createDate = LocalDateTime.now();
-
-    private LocalDateTime lastUpdateDate = LocalDateTime.now();
 
     public Long getId() {
         return id;
